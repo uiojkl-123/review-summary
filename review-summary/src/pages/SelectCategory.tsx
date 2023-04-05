cba1b2295ef0bc6c2ae19e5ae07f86bdf76d676c
uiojkl-123 <66120426+uiojkl-123@users.noreply.github.com>
2023-04-05 18:56:53 +0900

    :art: [structure] #4 edit page style by adding content container

--- review-summary/src/pages/SelectCategory.tsx
+++ review-summary/src/pages/SelectCategory.tsx
@@ -13,26 +13,26 @@ interface SelectCategoryProps {
 }
 
 export const SelectCategory: React.FC<SelectCategoryProps> = (props) => {
-    return (
-        <IonPage className="selectCategory">
-					<Container>
-							<div className="wrapper">
-								<BackBar />
-								<div className="contentContainer">
-									<h2 className="title">카테고리를 선택해 주세요</h2>
-									<div className="categoryList">
-										<CategoryItem name="블루투스 이어폰" imgUrl='bluetoothEarphone' />
-										<CategoryItem name="휴대폰 케이스" imgUrl='phoneCase' />
-										<CategoryItem name="보조배터리" imgUrl='battery' />
-										<CategoryItem name="노트북" imgUrl='laptop' />
-										<CategoryItem name="애플워치 스트랩" imgUrl='appleWatchStrap' />
-									</div>
-							</div>
-							</div>
-							<div className="footer">    
-									<Link to='./loading'><Button text={'시작하기'} /></Link>
-							</div>
-						</Container>
-        </IonPage>
-    );
+	return (
+		<IonPage className="selectCategory">
+			<Container>
+				<div className="wrapper">
+					<BackBar />
+					<div className="contentContainer">
+						<h2 className="title">카테고리를 선택해 주세요</h2>
+						<div className="categoryList">
+							<CategoryItem name="블루투스 이어폰" imgUrl='bluetoothEarphone' />
+							<CategoryItem name="휴대폰 케이스" imgUrl='phoneCase' />
+							<CategoryItem name="보조배터리" imgUrl='battery' />
+							<CategoryItem name="노트북" imgUrl='laptop' />
+							<CategoryItem name="애플워치 스트랩" imgUrl='appleWatchStrap' />
+						</div>
+					</div>
+				</div>
+				<div className="footer">
+					<Link to='./loading'><Button text={'시작하기'} /></Link>
+				</div>
+			</Container>
+		</IonPage>
+	);
 }
