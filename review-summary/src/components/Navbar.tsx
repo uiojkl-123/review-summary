7bbb7be1c3d5fc5c14430c095c15b76c9b60a5f7
KKWANH <kwanho0096@gmail.com>
2023-04-05 17:16:36 +0900

    :lipstick: [css] #1 navbar scss >> styled, root scss cleanup

--- review-summary/src/components/Navbar.tsx
+++ review-summary/src/components/Navbar.tsx
@@ -1,22 +1,67 @@
 import React from 'react';
 import './Navbar.scss';
+import styled from "@emotion/styled";
 
 interface NavbarProps {
 
 }
 
-export const Navbar: React.FC<NavbarProps> = (props) => {
+const NavbarWrapper = styled.div`
+width: 100%;
+max-width: 480px;
+height: 96px;
+margin: 0px auto;
+background: black;
+z-index: 100;
+`;
 
+const NavbarContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+height: 100%;
+`;
+
+const LogoContainer = styled.div`
+width: 96px;
+height: 96px;
+cursor: pointer;
+`;
+
+const Logo = styled.img`
+width: 100%;
+height: 100%;
+`;
+
+const TitleContainer = styled.div`
+font-size: 24px;
+font-weight: 700;
+color: white;
+margin: 0px;
+padding: 0px;
+margin-top: 32px;
+margin-left: 24px;
+`;
+
+const Title = styled.div`
+font-size: 2.3rem;
+height: 96px;
+margin: 0;
+`;
+
+export const Navbar: React.FC<NavbarProps> = (props) => {
     return (
-        <div className="navbar">
-            <div className="container">
-                <div className="logo">
-                    <img src="assets/logo.png" alt="" />
-                </div>
-                <div className="title">
-                    <h1>리뷰 요약</h1>
-                </div>
-            </div>
-        </div>
+        <NavbarWrapper className="navbar">
+            <NavbarContainer className="container">
+                <LogoContainer className="logo">
+                    <Logo src="assets/logo.png" alt="" />
+                </LogoContainer>
+                <TitleContainer className="title">
+                    <Title>
+											RevuGenius
+										</Title>
+                </TitleContainer>
+            </NavbarContainer>
+        </NavbarWrapper>
     );
 }
