03c0049f4da4dbfc3930c095b29e04b0c0b5e6df
KKWANH <kwanho0096@gmail.com>
2023-04-08 21:59:19 +0900

    :sparkes: [feature] #4 product sort feature added

---
+++ review-summary/src/components/products/sort.tsx
@@ -0,0 +1,7 @@
+export enum SortOptions {
+  Default   = "기본값",
+  PriceAsc  = "낮은 가격순",
+  PriceDesc = "높은 가격순",
+  ScoreAsc  = "낮은 평점순",
+  ScoreDesc = "높은 평점순",
+}
--- review-summary/src/pages/Products.tsx
+++ review-summary/src/pages/Products.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import styled from "@emotion/styled";
-
 import { IonPage } from '@ionic/react';
+
 import { Container } from '../theme/theme';
 import { Product } from '../models/model';
 import { ProductItem } from '../components/products/productItem';
 import { getProductsByCategory } from "../services/products.service"
+import { SortOptions } from '../components/products/sort';
 
 interface ProductsProps {
 }
@@ -22,10 +23,19 @@ height: calc(100vh - 40px);
 
 const TitleContainer = styled.div`
 width: 100%;
-height: 40px;
-background-color: rgba(0,0,0,0.7);
+height: 60px;
+background-color: #060606;
 display: flex;
 align-items: center;
+`;
+
+const SortContainer = styled.div`
+width: 100%;
+height: 60px;
+background-color: #121212;
+display: flex;
+align-items: center;
+justify-content: flex-end;
 `;
 
 const CancelButtonBox = styled.div`
@@ -45,6 +55,21 @@ justify-content: center;
 margin-left: -40px;
 `;
 
+const SortSelect = styled.select`
+  margin-right: 15px;
+  padding: 6px 24px 6px 8px;
+  border-radius: 14px;
+  background: transparent url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 10' width='12' height='6'%3E%3Cpath fill='%23ffffff' d='M4 7.5L8 0H0z'/%3E%3C/svg%3E") no-repeat right 0.5rem center;
+  border: 1px solid #ffffff;
+  appearance: none;
+  font-size: 14px;
+  color: #ffffff;
+	cursor: pointer;
+  &:focus {
+    outline: none;
+  }
+`;
+
 export const Products: React.FC<ProductsProps> = (props) => {
 	const [products, setProducts] = useState<Product[]>([]);
 	const param = useParams<any>()
@@ -58,6 +83,27 @@ export const Products: React.FC<ProductsProps> = (props) => {
 		})();
 	}, [category]);
 
+	const sortProducts = (option: SortOptions) => {
+		let sortedProducts = [...products];
+		switch (option) {
+			case SortOptions.PriceAsc:
+				sortedProducts.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+				break;
+			case SortOptions.PriceDesc:
+				sortedProducts.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+				break;
+			case SortOptions.ScoreAsc:
+				sortedProducts.sort((a, b) => parseFloat(a.score) - parseFloat(b.score));
+				break;
+			case SortOptions.ScoreDesc:
+				sortedProducts.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+				break;
+			default:
+				break;
+		}
+		setProducts(sortedProducts);
+	};
+
   return (
     <IonPage className="products">
       <TitleContainer>
@@ -68,6 +114,17 @@ export const Products: React.FC<ProductsProps> = (props) => {
           {category}
         </Title>
       </TitleContainer>
+			<SortContainer>
+				<SortSelect
+					onChange={(e) => sortProducts(e.target.value as SortOptions)}
+				>
+					{Object.entries(SortOptions).map(([key, value]) => (
+						<option key={key} value={key}>
+							{value}
+						</option>
+					))}
+				</SortSelect>
+      </SortContainer>
       <ProductContainer>
         {products.map((product) => (
 					<>
