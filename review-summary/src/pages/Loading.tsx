9c4adcc19bc9e717a1be87b8d7d7e0c067d982e6
KKWANH <kwanho0096@gmail.com>
2023-04-09 13:16:01 +0900

    :sparkles: [feature] #6 animation css by gpt

--- review-summary/src/pages/Loading.tsx
+++ review-summary/src/pages/Loading.tsx
@@ -1,40 +1,74 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import './Loading.scss';
 import { IonPage } from '@ionic/react';
 import { BackBar } from '../components/BackBar';
 import { Button } from '../components/Button';
 import { Link, useLocation } from 'react-router-dom';
 import { Container } from '../theme/theme';
+import styled from '@emotion/styled';
 
-interface LoadingProps {
+interface LoadingProps {}
 
-}
+const RippleContainer = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+  border: 1px solid #ccc;
+  cursor: pointer;
+`;
 
 export const Loading: React.FC<LoadingProps> = (props) => {
+  const state = useLocation().state as any;
+  const category = state?.category;
 
+  const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    const ripple = document.createElement("div");
+    ripple.className = "ripple";
+    const diameter = Math.max(e.currentTarget.clientWidth, e.currentTarget.clientHeight);
+    const radius = diameter / 2;
 
-	const state = useLocation().state as any;
+    ripple.style.width = `${diameter}px`;
+    ripple.style.height = `${diameter}px`;
+    ripple.style.borderRadius = '50%';
+    ripple.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+    ripple.style.position = 'absolute';
+    ripple.style.transform = 'scale(0)';
+    ripple.style.animation = 'ripple 1s ease-out';
 
-	const category = state?.category;
+    const x = e.clientX - e.currentTarget.getBoundingClientRect().left - radius;
+    const y = e.clientY - e.currentTarget.getBoundingClientRect().top - radius;
 
-	console.log(category);
+    ripple.style.left = `${x}px`;
+    ripple.style.top = `${y}px`;
 
-	return (
-		<IonPage className="loading">
-			<BackBar />
-			<Container>
-				<div className="wrapper">
-					<div className="contentContainer">
-						<h2 className="title">정보를 불러오고 있어요!<br />잠시만 기다려주세요.</h2>
-						<div className="loadingImage">
-							<img src="loading" alt="" />
-						</div>
-					</div>
-				</div>
-				<div className="footer">
-					<Link to={{ pathname: '/products/' + category }}> <Button>상품보기</Button></Link>
-				</div>
-			</Container>
-		</IonPage >
-	);
+    e.currentTarget.appendChild(ripple);
+
+    setTimeout(() => {
+      ripple.remove();
+    }, 1000);
+  };
+
+  return (
+    <IonPage className="loading">
+      <BackBar />
+      <Container>
+        <div className="wrapper">
+          <div className="contentContainer">
+            <h2 className="title">정보를 불러오고 있어요!<br />잠시만 기다려주세요.</h2>
+            <RippleContainer onClick={handleClick}>
+              <img src="loading" alt="" />
+            </RippleContainer>
+          </div>
+        </div>
+        <div className="footer">
+          <Link to={{ pathname: '/products/' + category }}> <Button>상품보기</Button></Link>
+        </div>
+      </Container>
+    </IonPage >
+  );
 }
